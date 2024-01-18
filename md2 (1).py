aa317779b113be55c38bab8daf950fce94628588
MicroEngineer <61626411+Akash-Kharat@users.noreply.github.com>
2024-01-18 13:01:40 +0530

    Update md2 (1).py

--- md2 (1).py
+++ md2 (1).py
@@ -5,7 +5,7 @@ import time
 import paho.mqtt.client as mqtt
 import json
 # Define the Modbus settings
-client = ModbusClient(method='rtu', port='/dev/ttyAMA0',  parity='N', baudrate=9600, bytesize=8, stopbits=1, timeout=2, strict=False)
+client = ModbusClient(method='rtu', port='/dev/ttyUSB0',  parity='N', baudrate=9600, bytesize=8, stopbits=1, timeout=2, strict=False)
 client.connect()
 # Slave IDs to read data from
 slave_ids = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,127]
@@ -27,21 +27,23 @@ def on_message(client, userdata, msg):
         # Extract values from JSON data
         print(data)
         if msg.topic == "transmonk/mechmark/site1/command":
+            if data.get("masterid") == "2":
             # Handle messages for topic1
             # Example: Do something with data from topic1
-            register_address = 5
-            values = data.get("workingvoltage")
-            slave_addr = data.get("to")
-            write_to_registers(register_address, int(values), int(slave_addr))
-            pass
+                register_address = 5
+                values = data.get("workingvoltage")
+                slave_addr = data.get("to")
+                write_to_registers(register_address, int(values), int(slave_addr))
+                pass
         elif msg.topic == "transmonk/mechmark/site1/reconfigure":
+            if data.get("masterid") == "2":
             # Handle messages for topic2
             # Example: Do something with data from topic2
-            register_address = 0
-            values = data.get("newid")
-            slave_addr = 127
-            write_to_registers(register_address, int(values), int(slave_addr))
-            pass
+                register_address = 0
+                values = data.get("newid")
+                slave_addr = 127
+                write_to_registers(register_address, int(values), int(slave_addr))
+                pass
         else:
             # Handle other topics if needed
             print(f"invalid topic: {msg.topic}")
@@ -55,7 +57,7 @@ client_mqtt = mqtt.Client()
 client_mqtt.on_connect = on_connect
 client_mqtt.on_message = on_message
 # Connect to the MQTT broker
-client_mqtt.connect("dev.coppercloud.in", 1883, 60)  # Replace with your MQTT broker information
+client_mqtt.connect("transmonkmqtt.coppercloud.in", 1883, 60)  # Replace with your MQTT broker information
 # Start the MQTT loop in a separate thread
 mqtt_thread = threading.Thread(target=client_mqtt.loop_start, daemon=True)
 mqtt_thread.start()
@@ -66,7 +68,7 @@ def read_registers():
 
             try:
                 # Specify the slave address and the range of registers to read
-                response = client.read_holding_registers(address=0, count=22, unit=slave_id)
+                response = client.read_holding_registers(address=0, count=22, slave=slave_id)
                 # Process the data if the response is valid
                 if response.isError():
                     # Handle Modbus exceptions
@@ -78,7 +80,7 @@ def read_registers():
                     values = response.registers
                     print(f"Slave {slave_id} Values: {values}")
                     # Publish values to MQTT
-                    client_mqtt.publish("transmonk/mechmark/data", json.dumps({"masterid":1, "deviceid": slave_id, "values": values}))  # Replace with your MQTT topic
+                    client_mqtt.publish("transmonk/mechmark/site1/data", json.dumps({"masterid":1, "deviceid": slave_id, "values": values}))  # Replace with your MQTT topic
                     time.sleep(3)
             except ModbusException as e:
                 # Handle Modbus exceptions
@@ -96,7 +98,7 @@ def write_to_registers(register_address , values, unit):
 
         try:
             # Specify the slave address, register address, and values to write
-            client.write_registers(register_address, values, unit=unit)
+            client.write_registers(register_address, values, slave=unit)
             print(f"Write successful to Slave {unit}, Address {register_address}, Values {values}")
             time.sleep(3)
         except ModbusException as e:
